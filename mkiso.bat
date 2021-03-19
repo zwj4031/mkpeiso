ac051937f14bdf982c47fa37586d34e481d6e273
zwj4031 <dnyl@qq.com>
2021-03-19 13:53:43 +0800

    fix bug

--- mkiso.bat
+++ mkiso.bat
@@ -1,5 +1,6 @@
+:::: -boot-load-size 4为grldr之类，4k以上的文件填8 或取消参数
 cd /d %~dp0
 echo . build\ventoy.dat
-%~dp0bin\mkisofs -udf -v -V ChobitPE -no-emul-boot -boot-load-size 4 -b boot/etfsboot.com -eltorito-platform efi -b efi/microsoft/boot/efisys_noprompt.bin -o chobitpe.iso -R -U build
+%~dp0bin\mkisofs.exe -udf -d -v -V ChobitPE -no-emul-boot -b boot/etfsboot.com -eltorito-platform efi -b efi/microsoft/boot/efisys_noprompt.bin -o chobitpe.iso -R -U build
 echo ok.
 exit
--- mkiso.sh
+++ mkiso.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 touch build/ventoy.dat
-mkisofs -udf -boot.catalog -b boot/etfsboot.com  -v -V ChobitPE -e efi/microsoft/boot/efisys_noprompt.bin -no-emul-boot -o chobitpe.iso -R -U build
+#mkisofs -udf -d -v -V ChobitPE -no-emul-boot -boot-load-size 8 -b boot/etfsboot.com -e efi/microsoft/boot/efisys_noprompt.bin -o chobitpe.iso -R -U build
+rm -rf chobitpe.iso
+xorriso -as mkisofs -R -hide-joliet boot.catalog -boot-load-size 8 -b boot/etfsboot.com  -no-emul-boot -allow-lowercase -boot-load-size 8 -boot-info-table \
+-eltorito-alt-boot -e efi/microsoft/boot/efisys_noprompt.bin -no-emul-boot -o chobitpe.iso build
