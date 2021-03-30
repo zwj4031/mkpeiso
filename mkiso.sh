6f6ab3f27cee21619ee83d74b452629c64ab49e8
zwj4031 <dnyl@qq.com>
2021-03-30 21:58:22 +0800

    bios->grldr

--- mkiso.sh
+++ mkiso.sh
@@ -2,5 +2,5 @@
 touch build/ventoy.dat
 #mkisofs -udf -d -v -V ChobitPE -no-emul-boot -boot-load-size 8 -b boot/etfsboot.com -e efi/microsoft/boot/efisys_noprompt.bin -o chobitpe.iso -R -U build
 rm -rf chobitpe.iso
-xorriso -as mkisofs -R -hide-joliet boot.catalog -boot-load-size 8 -b boot/etfsboot.com  -no-emul-boot -allow-lowercase -boot-load-size 8 -boot-info-table \
+xorriso -as mkisofs -R -hide-joliet boot.catalog -boot-load-size 4 -b boot/grldr -no-emul-boot -allow-lowercase -boot-info-table \
 -eltorito-alt-boot -e efi/microsoft/boot/efisys_noprompt.bin -no-emul-boot -o chobitpe.iso build
